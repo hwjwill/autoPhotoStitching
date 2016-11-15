376bb854c211c38f87a3e61f7d8d0b150853bb20
Will Huang <hwjwill@berkeley.edu>
2016-11-14 23:11:44 -0800

    finished part 1

---
+++ README.md
@@ -0,0 +1,12 @@
+# (Auto)Photo Stitching
+
+This project aimes to explore applications of homography transformation. In this part, we created image rectification and image stitching. More results can be found [here](https://inst.eecs.berkeley.edu/~cs194-26/fa16/upload/files/proj7A/cs194-26-acm/)
+
+![Example](https://inst.eecs.berkeley.edu/~cs194-26/fa16/upload/files/proj7A/cs194-26-acm/mStitch.jpg)
+
+# To run code
+
+Please see comments in main.m for further definition of parameter
+```
+main()
+``` 
--- main.m
+++ main.m
@@ -20,10 +20,14 @@ if part ==1
     imwrite(result, filename);
 elseif part == 2
     %% Image mosaic
-    imname = 'doe.jpg';
-    imname2 = 'doe2.jpg';
-    im2csv = 'doe2Points.csv';
-    basecsv = 'doePoints.csv';
+    % imname is the image name of the base image (the image that is not 
+    % going to be wraped). Imname2 is the image name of wrap image. CSV
+    % files contain the conrespondence. 
+    imname = 'ms.jpg';
+    imname2 = 'm3.jpg';
+    im2csv = 'm3Points.csv';
+    basecsv = 'msPoints.csv';
+    filename = 'mStitch.jpg';
     
     im2pts = csvread(im2csv);
     im2pts = [im2pts(:,2), im2pts(:,1)];
@@ -32,26 +36,12 @@ elseif part == 2
     imbase = imread(imname);
     im2 = imread(imname2);
     H = computeH(im2pts, basepts);
-    result = warpImage(im2, H, size(imbase, 1), size(imbase, 2));
-    result = result * 255;
-%     boo1 = (rgb2gray(result) > 0);
-%     boo2 = (rgb2gray(imbase) > 0);
-%     mask = boo1 + boo2;
-%     intersect = mask > 1;
-%     mask(intersect) = 0.5;
+    result = single(warpImage(im2, H, size(imbase, 1), size(imbase, 2)));
+    result(isnan(result)) = 0;
     mask = zeros(size(result(:, :, 1)));
-    mask(:, size(mask, 2) / 2 : end) = 1;
-    final = blend(double(imbase), result, mask);
-    imshow(final);
-    %imshow(result);
-%     imname3 = 'doe3.jpg';
-%     
-%     im3 = imread(imname3);
-%     result = zeros(size(im3, 1) * 3, size(im3, 2) * 3, 3);
-%     result(round(size(result, 1) * 0.33):round(size(result, 1) * 0.33) + 599,...
-%         round(size(result, 2) * 0.5):round(size(result, 2) * 0.5) + 799, :) = ...
-%         im3;
-%     imwrite(uint8(result), 'doe.jpg');
+    mask(:, 1 : size(mask, 2) / 2) = 1;
+    final = blend(im2single(imbase), result, mask);
+    imwrite(final, filename);
 end
 end
 
@@ -125,7 +115,6 @@ f = exp(-x.^2/(2*sigma^2) - y.^2 / (2*sigma^2));
 f = f ./ sum(f(:));
 end
 
-
 function [result] = blend(im1, im2, mask1)
 im1r = im1(:, :, 1);
 im1g = im1(:, :, 2);
@@ -134,14 +123,14 @@ im2r = im2(:, :, 1);
 im2g = im2(:, :, 2);
 im2b = im2(:, :, 3);
 mask2 = ones(size(mask1)) - mask1;
-N = 1;
+N = 2;
 lowIm1r = im1r;
 lowIm1g = im1g;
 lowIm1b = im1b;
 lowIm2r = im2r;
 lowIm2g = im2g;
 lowIm2b = im2b;
-sigma = 1;
+sigma = 10;
 resultr = zeros(size(im1r));
 resultg = zeros(size(im1g));
 resultb = zeros(size(im1b));
@@ -185,5 +174,5 @@ end
 resultr = resultr + low1r .* tempMask1 + low2r .* tempMask2;
 resultg = resultg + low1g .* tempMask1 + low2g .* tempMask2;
 resultb = resultb + low1b .* tempMask1 + low2b .* tempMask2;
-result = uint8(cat(3, resultr, resultg, resultb));
+result = cat(3, resultr, resultg, resultb);
 end
